a513fa45a97308ffa54d5d7fd94b4ef04b78ba19
GitHub Action <action@github.com>
2020-09-17 07:24:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/taideli/gitlabsupport/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/taideli/gitlabsupport/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.taideli.gitlabsupport.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.taideli.gitlabsupport.MyBundle
 
 class MyApplicationService {
 
